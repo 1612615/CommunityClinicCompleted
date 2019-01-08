6782e9ac13ab0dc70f8346323def6e34a9a4aef2
Amrie69 <amrieibifc-wa16@student.tarc.edu.my>
2019-01-08 11:27:01 +0800

    Addition of Appointment Related Activities

---
+++ app/src/main/java/com/google/communityclinic/Appointment.java
@@ -0,0 +1,45 @@
+package com.google.communityclinic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Appointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+    }
+    public void makeNewAppointment (View view) {
+        Intent intent = new Intent(this, NewAppointment.class);
+        startActivity(intent);
+    }
+
+    public void appointmentRemainder (View view) {
+        Intent intent = new Intent(this, Remainder.class);
+        startActivity(intent);
+    }
+
+    public void appoinmentHistory (View view) {
+        Intent intent = new Intent(this, History.class);
+        startActivity(intent);
+    }
+
+}
