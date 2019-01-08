207b4e4b69c3d5ede295ca5b89de451485775938
Amrie69 <amrieibifc-wa16@student.tarc.edu.my>
2019-01-08 11:27:32 +0800

    Addition of Appointment Related Activities

--- app/src/main/java/com/google/communityclinic/MainActivity.java
+++ app/src/main/java/com/google/communityclinic/MainActivity.java
@@ -1,5 +1,6 @@
 package com.google.communityclinic;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -89,5 +91,10 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void appointment (View view) {
+        Intent intent = new Intent(this, Appointment.class);
+        startActivity(intent);
     }
 }
